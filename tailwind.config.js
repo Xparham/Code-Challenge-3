eb78828bf92b44916589e65d7561ef329556d86f
Xparham <xparham@gmail.com>
2021-01-19 14:45:58 -0500

    pushed to github

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+        backgroundImage: theme => ({
+         'hero-pattern': "url('https://news.airbnb.com/wp-content/uploads/sites/4/2020/05/Airbnb-Singaraja-Bali-Indonesia.jpg')"
+    
+        })
+      }
+    }
+  }
